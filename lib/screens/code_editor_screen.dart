e01655d3cdb55e1e7e269f22e40c36508a603030
Alisherro <good071104@gmail.com>
2022-11-24 15:39:23 +0600

    routing

---
+++ lib/screens/code_editor_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class CodeEditorScreen extends StatelessWidget {
+  const CodeEditorScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl: 'https://flutter.dev',
+      onWebResourceError: (WebResourceError webviewerrr) {
+        print("Handle your Error Page here");
+      },
+    );
+  }
+}
